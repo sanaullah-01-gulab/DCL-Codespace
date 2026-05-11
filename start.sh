bcc54df9cfe2d2ce7cdd1c3eb8a541e46956446d
sanaullah-01-gulab <sanaullahgulab1@gmail.com>
2026-05-11 22:12:34 +0000

    Changed Front-end and updated Start shell file

--- start.sh
+++ start.sh
@@ -3,3 +3,6 @@ echo "Starting DCL containers..."
 cd /workspaces/DCL-Codespace
 docker compose -f pwd.yml start
 echo "✅ Done! Open: https://$CODESPACE_NAME-8080.app.github.dev"
+sleep 15
+docker compose -f pwd.yml exec backend pip install -e /home/frappe/frappe-bench/apps/delta_cables
+echo "✅ Done!"
